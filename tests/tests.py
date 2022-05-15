a4e76f289c29bb7ab888b743fd3593b4a8ba96b7
naaga96 <91053226+naaga96@users.noreply.github.com>
2022-05-16 00:34:25 +0200

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -4,13 +4,14 @@ sys.path.insert(0, 'BrainAge')
 #import os
 #import numpy as np
 
-from features import Utilities as util
+from features import Utilities
 
 class TestBrainAge(unittest.TestCase):
 	'''
 	Class for testing our code
 	'''
 	def test_file_reader(self):
+		util = Utilities()
   		dataframe = util.file_reader(file_url = 'https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
   		assert dataframe.size == 387960
   		assert dataframe.shape == (915, 424)
