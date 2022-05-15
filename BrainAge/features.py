3f593035efee00a55ca63490cc9cda5cb461a9f9
naaga96 <agata.minnocci@gmail.com>
2022-05-15 23:04:59 +0200

    more test developement

--- BrainAge/features.py
+++ BrainAge/features.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
-def file_reader(file_url):
-  '''
-  Read data features from url and return them in a dataframe
-  '''
-  dataset_file = file_url
-  dataframe = pd.read_csv(dataset_file, sep=';')
-  return dataframe
+class Utilities():
+	'''
+	Basic utilities functions for data
+	'''
+	
+	def __init__(self, df):
+        """
+        Initialize the class.
+        """
+        self.df= df
+        
+	def file_reader(self, file_url):
+  	'''
+ 	Read data features from url and return them in a dataframe
+  	'''
+  	dataset_file = file_url
+  	self.df = pd.read_csv(dataset_file, sep=';')
+  	return self.df
--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,17 @@
 import unittest
 import sys
-sys.path.insert(0, 'BrainAge/')
+sys.path.insert(0, 'BrainAge')
 #import os
 #import numpy as np
 
-import BrainAge.features as ft
+from features import Utilities
 
 class TestBrainAge(unittest.TestCase):
 	"""
 	Class for testing our code
 	"""
 	def test_file_reader():
-  		dataframe=ft.file_reader('https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
+  		dataframe=util.file_reader('https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
   		assert dataframe.size == 387960
   		assert dataframe.shape == (915, 424)
 if __name__=='__main__':
