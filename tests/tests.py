e8e4177abd8c848986fde517bf5103c351d9c408
naaga96 <91053226+naaga96@users.noreply.github.com>
2022-05-16 00:36:40 +0200

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -12,8 +12,8 @@ class TestBrainAge(unittest.TestCase):
 	'''
 	def test_file_reader(self):
 		util = Utilities()
-  		dataframe = util.file_reader(file_url = 'https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
-  		assert dataframe.size == 387960
-  		assert dataframe.shape == (915, 424)
+		dataframe = util.file_reader(file_url = 'https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
+		assert dataframe.size == 387960
+		assert dataframe.shape == (915, 424)
 if __name__=='__main__':
 	unittest.main()
