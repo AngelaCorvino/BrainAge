14e7a90dabf1f3120e8827db18c79ef1afa0c21f
naaga96 <91053226+naaga96@users.noreply.github.com>
2022-05-15 23:10:50 +0200

    Update tests.py
    more indentation problems

--- tests/tests.py
+++ tests/tests.py
@@ -7,12 +7,12 @@ sys.path.insert(0, 'BrainAge')
 from features import Utilities
 
 class TestBrainAge(unittest.TestCase):
-	"""
+	'''
 	Class for testing our code
-	"""
+	'''
 	def test_file_reader():
   		dataframe=util.file_reader('https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
   		assert dataframe.size == 387960
   		assert dataframe.shape == (915, 424)
 if __name__=='__main__':
-   unittest.main()
+	unittest.main()
