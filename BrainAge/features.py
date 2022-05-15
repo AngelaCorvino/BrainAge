05768ed83c2f770cfb29d36b5f3920e0bb644f03
naaga96 <91053226+naaga96@users.noreply.github.com>
2022-05-15 23:16:28 +0200

    Update features.py
    hopefully last indentation problem

--- BrainAge/features.py
+++ BrainAge/features.py
@@ -6,15 +6,15 @@ class Utilities():
 	'''
 	
 	def __init__(self, df):
-        '''
-        Initialize the class.
-        '''
-        	self.df= df
+		'''
+		Initialize the class.
+		'''
+		self.df= df
         
 	def file_reader(self, file_url):
-  	'''
- 	Read data features from url and return them in a dataframe
-  	'''
-  		dataset_file = file_url
-  		self.df = pd.read_csv(dataset_file, sep=';')
-  		return self.df
+		'''
+		Read data features from url and return them in a dataframe
+		'''
+		dataset_file = file_url
+		self.df = pd.read_csv(dataset_file, sep=';')
+		return self.df
