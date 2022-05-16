5ceb05105cf8c9d3ca564a000696aaafeb995d69
a.corvino@hzdr.de <gelato20>
2022-05-16 13:00:36 +0200

    New unit test.

--- BrainAge/features.py
+++ BrainAge/features.py
@@ -1,21 +1,33 @@
 import pandas as pd
 
-class Utilities():
-	'''
-	Basic utilities functions for data
-	'''
 
-	def __init__(self,file_url):
-		'''
-		Initialize the class.
-		'''
-		self.file_url = file_url
-		self.df = file_reader()
+class Utilities:
+    """
+    Basic utilities functions for data
+    """
 
-	def file_reader(self):
-		'''
-		Read data features from url and return them in a dataframe
-		'''
-		dataset_file = self.file_url
-		df = pd.read_csv(dataset_file, sep=';')
-		return df
+    def __init__(self, file_url):
+        """
+        Initialize the class.
+        """
+        self.file_url = file_url
+        self.df = file_reader()
+        (self.df_AS, self.df_TD) = file_split()
+
+    def file_reader(self):
+        """
+        Read data features from url and return them in a dataframe
+        """
+        dataset_file = self.file_url
+        df = pd.read_csv(dataset_file, sep=";")
+		df['Site'] =df.FILE_ID.apply(lambda x: x.split('_')[0])
+        return df
+
+
+    def file_split(self):
+        """
+        Split dataframe in healthy(control) and autistic subjects
+        """
+        df_AS = self.df.loc[df.DX_GROUP == 1]
+        df_TD = self.df.loc[df.DX_GROUP == -1]
+        return df_AS, df_TD
--- tests/tests.py
+++ tests/tests.py
@@ -13,7 +13,18 @@ class TestBrainAge(unittest.TestCase):
 	def test_file_reader(self):
 		util = Utilities('https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
 		dataframe = util.file_reader()
-		assert dataframe.size == 387960
-		assert dataframe.shape == (915, 424)
+		df_AS,df_TD=util.file_split()
+		assert dataframe.size == 388875
+		assert dataframe.shape == (915, 425)
+		assert df_AS.shape == (451, 425)
+		assert df_TD.shape == (464, 425)
+
+	def test_file_split(self):
+		util = Utilities('https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
+		df_AS,df_TD=util.file_split()
+		assert df_AS.shape == (451, 425)
+		assert df_TD.shape == (464, 425)
+
+
 if __name__=='__main__':
 	unittest.main()
