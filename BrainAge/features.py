84f80ab8172081064fb0456f375e110db71cd265
a.corvino@hzdr.de <gelato20>
2022-05-16 12:37:45 +0200

    Features.py and test.py changed

--- BrainAge/features.py
+++ BrainAge/features.py
@@ -4,17 +4,18 @@ class Utilities():
 	'''
 	Basic utilities functions for data
 	'''
-	
-	def __init__(self):
+
+	def __init__(self,file_url):
 		'''
 		Initialize the class.
 		'''
-		#self.df= df
-        
-	def file_reader(self, file_url):
+		self.file_url = file_url
+		self.df = file_reader(self.file_url)
+
+	def file_reader(self):
 		'''
 		Read data features from url and return them in a dataframe
 		'''
-		dataset_file = file_url
+		dataset_file = self.file_url
 		df = pd.read_csv(dataset_file, sep=';')
 		return df
--- tests/tests.py
+++ tests/tests.py
@@ -11,8 +11,8 @@ class TestBrainAge(unittest.TestCase):
 	Class for testing our code
 	'''
 	def test_file_reader(self):
-		util = Utilities()
-		dataframe = util.file_reader(file_url = 'https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
+		util = Utilities('https://raw.githubusercontent.com/AngelaCorvino/BrainAge/main/BrainAge/data/FS_features_ABIDE_males.csv')
+		dataframe = util.file_reader()
 		assert dataframe.size == 387960
 		assert dataframe.shape == (915, 424)
 if __name__=='__main__':
