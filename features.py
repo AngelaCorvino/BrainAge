58ebf7beaf0276d7bab1525e22d34ef21eb82006
naaga96 <agata.minnocci@gmail.com>
2022-05-16 12:20:24 +0200

    adding library to work on

---
+++ features.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+class Utilities():
+	'''
+	Basic utilities functions for data
+	'''
+	
+	def __init__(self):
+		'''
+		Initialize the class.
+		'''
+		#self.df= df
+        
+	def file_reader(self, file_url):
+		'''
+		Read data features from url and return them in a dataframe
+		'''
+		dataset_file = file_url
+		df = pd.read_csv(dataset_file, sep=';')
+		return df
