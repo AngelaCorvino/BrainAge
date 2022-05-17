42bdc2b0e4e653106de82920dfe36184358b9cec
naaga96 <agata.minnocci@gmail.com>
2022-05-17 20:08:50 +0200

    added some functions and new trial for tests

--- BrainAge/features.py
+++ BrainAge/features.py
@@ -1,11 +1,10 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 class Utilities:
     """
     Basic utilities functions for data
     """
-
     def __init__(self, file_url):
         """
         Initialize the class.
@@ -18,16 +17,38 @@ class Utilities:
         """
         Read data features from url and return them in a dataframe
         """
-        dataset_file = self.file_url
-        df = pd.read_csv(dataset_file, sep=";")
-        df['Site'] =df.FILE_ID.apply(lambda x: x.split('_')[0])
+        df = pd.read_csv(self.file_url, sep = ";")
         return df
-
+        
+    def add_features(self):
+        """
+        Add columns with derived features
+        """
+        self.df['TotalWhiteVol'] = self.df.lhCerebralWhiteMatterVol+self.df.rhCerebralWhiteMatterVol
+        self.df['Site'] = self.df.FILE_ID.apply(lambda x: x.split('_')[0])
+        return
 
     def file_split(self):
         """
-        Split dataframe in healthy(control) and autistic subjects
+        Split dataframe in healthy (control) and autistic subjects groups
         """
         df_AS = self.df.loc[self.df.DX_GROUP == 1]
         df_TD = self.df.loc[self.df.DX_GROUP == -1]
         return df_AS, df_TD
+        
+    def plot_histogram(self, feature, control = True):
+        """
+        Plot histogram of a given feature on the indicated group, masking values <0
+        """
+        if control == True:
+            self.df_TD[self.df_TD.loc[:, feature]>0].hist([feature])
+        elif control == False:
+            self.df_ASD[self.gdf_ASD.loc[:, feature]>0].hist([feature])
+        plt.show()
+        return
+        
+if __name__ == "__main__":
+    util = Utilities("data/FS_features_ABIDE_males.csv")
+    util.add_features()
+    util.file_split()
+    util.plot_histogram('AGE_AT_SCAN')
--- tests/tests.py
+++ tests/tests.py
@@ -2,12 +2,12 @@ import unittest
 import os
 import sys
 
-package_name = "BrainAge"
+package_name = "../BrainAge"
 # package_root = os.path.abspath("..")
 # sys.path.insert(0, package_root)
 # sys.path.insert(0, os.path.join(package_root, package_name))
 
-sys.path.insert(0,package_name)
+sys.path.insert(0, package_name)
 
 from features import Utilities
 from regression import Regression
@@ -17,31 +17,28 @@ class TestBrainAge(unittest.TestCase):
     """
     Class for testing our code
     """
-
-
     def test_file_reader(self):
         util = Utilities(
-            "/data/FS_features_ABIDE_males.csv"
+            "../BrainAge/data/FS_features_ABIDE_males.csv"
         )
         dataframe = util.file_reader()
         df_AS, df_TD = util.file_split()
-        assert dataframe.size == 388875
-        assert dataframe.shape == (915, 425)
-        assert df_AS.shape == (451, 425)
-        assert df_TD.shape == (464, 425)
-
+        assert dataframe.size == 387960
+        assert dataframe.shape == (915, 424)
+        assert df_AS.shape == (451, 424)
+        assert df_TD.shape == (464, 424)
 
     def test_file_split(self):
         util = Utilities(
-            "/data/FS_features_ABIDE_males.csv"
+            "../BrainAge/data/FS_features_ABIDE_males.csv"
         )
         df_AS, df_TD = util.file_split()
-        assert df_AS.shape == (451, 425)
-        assert df_TD.shape == (464, 425)
+        assert df_AS.shape == (451, 424)
+        assert df_TD.shape == (464, 424)
 
     def test_feature_selection(self):
         util = Regression(
-            "/data/FS_features_ABIDE_males.csv"
+            "../BrainAge/data/FS_features_ABIDE_males.csv"
         )
         features = util.feature_selection(heatmap=False)
         assert features.shape == (16, )
