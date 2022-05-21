9caf4dd50623fe71b01508cad51aa018dc92e62a
naaga96 <agata.minnocci@gmail.com>
2022-05-21 18:16:50 +0200

    adding a test

--- tests/tests.py
+++ tests/tests.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 #to run locally
-#package_name = "../BrainAge" 
-package_name = "BrainAge"
+package_name = "../BrainAge" 
+#package_name = "BrainAge"
 
 
 sys.path.insert(0, package_name)
@@ -29,18 +29,25 @@ class TestBrainAge(unittest.TestCase):
         df_AS, df_TD = util.file_split()
         assert dataframe.size == 387960
         assert dataframe.shape == (915, 424)
-        assert df_AS.shape == (451, 424)
-        assert df_TD.shape == (464, 424)
+        assert df_AS.shape == (451, 426)
+        assert df_TD.shape == (464, 426)
+        
+    def test_add_feature(self):
+        util = Utilities(self.data)
+        dataframe = util.file_reader()
+        dataframe = util.add_features()
+        assert 'Site' in dataframe.keys()
+        assert 'TotalWhiteVol' in dataframe.keys()
 
     def test_file_split(self):
         util = Utilities(self.data)
         df_AS, df_TD = util.file_split()
-        assert df_AS.shape == (451, 424)
-        assert df_TD.shape == (464, 424)
+        assert df_AS.shape == (451, 426)
+        assert df_TD.shape == (464, 426)
 
     def test_feature_selection(self):
-        util = Regression(self.data)
-        features = util.feature_selection(heatmap=False)
+        util = Utilities(self.data)
+        features = util.feature_selection('AGE_AT_SCAN', False)
         assert features.shape == (16, )
 
 if __name__ == "__main__":
