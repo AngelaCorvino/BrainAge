136824a00d5b668f9577c2a41e69f4061699dea4
naaga96 <agata.minnocci@gmail.com>
2022-05-21 22:57:06 +0200

    adding tests and functions to regression

--- BrainAge/regression.py
+++ BrainAge/regression.py
@@ -7,15 +7,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import RobustScaler
 from sklearn.model_selection import KFold
-from sklearn.svm import SVC
+
+from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.ensemble import RandomForestRegressor
+
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 # from sklearn.preprocessing import PolynomialFeatures
-
 
 from features import Utilities
 
@@ -53,7 +55,7 @@ class Regression():
             pass
         return X,y
 
-    def k_Fold(self,n_splits,model):
+    def k_Fold(self, n_splits, model):
         """
         Split the data and test it on a model chosen by the user
         """
@@ -72,18 +74,63 @@ class Regression():
             MSE=mean_squared_error(self.y[test_index], predict_y, squared=False)
             MAE=mean_absolute_error(self.y[test_index], predict_y)
         return predict_y,MSE,MAE
+        
+        
+""" Pretty functions"""       
+   
+def pretty(A, w=None, h=None):
+    if A.ndim==1:
+        if w == None :
+            return str(A)
+        else:
+            s ='['+' '*(max(w[-1],len(str(A[0])))-len(str(A[0]))) +str(A[0])
+            for i,AA in enumerate(A[1:]):
+                s += ' '*(max(w[i],len(str(AA)))-len(str(AA))+1)+str(AA)
+            s +='] '
+    elif A.ndim==2:
+        w1 = [max([len(str(s)) for s in A[:,i]])  for i in range(A.shape[1])]
+        w0 = sum(w1)+len(w1)+1
+        s= u'\u250c'+u'\u2500'*w0+u'\u2510' +'\n'
+        for AA in A:
+            s += ' ' + pretty(AA, w=w1) +'\n'    
+        s += u'\u2514'+u'\u2500'*w0+u'\u2518'
+    elif A.ndim==3:
+        h=A.shape[1]
+        s1=u'\u250c' +'\n' + (u'\u2502'+'\n')*h + u'\u2514'+'\n'
+        s2=u'\u2510' +'\n' + (u'\u2502'+'\n')*h + u'\u2518'+'\n'
+        strings=[pretty(a)+'\n' for a in A]
+        strings.append(s2)
+        strings.insert(0,s1)
+        s='\n'.join(''.join(pair) for pair in zip(*map(str.splitlines, strings)))
+    return s
+    
+def make_pretty(func): #esperimento con i decoratori
+    def inner(A, *args):
+        print((pretty((np.array(A)))))
+        return func(A)
+    return inner
+    
+"""Functions for future main"""
 
+models = [LinearRegression(), GaussianProcessRegressor(), RandomForestRegressor(), Lasso(), SVR()]
 
-
-
+def run_models(models, model_results = []):
+    print(models)
+    a = Regression("data/FS_features_ABIDE_males.csv")
+    a.rescale('Robust')
+    for model in models:
+    	predict_age, MSE, MAE = a.k_Fold(10, model)
+    	model_results.append([MSE, MAE])
+    print(pretty((np.array(model_results))))
+    return model_results
 
 
 if __name__ == "__main__":
-    a=Regression("data/FS_features_ABIDE_males.csv")
-    a.rescale('Robust')
-    predict_age,MSE,MAE=a.k_Fold(10,LinearRegression())
-    print(predict_age,MSE,MAE)
-
-    predict_age,MSE,MAE=a.k_Fold(10,GaussianProcessRegressor())
-    a.rescale('Standard')
-    print(predict_age,MSE,MAE)
+    run_models(models, [])
+    #a=Regression("data/FS_features_ABIDE_males.csv")
+    #a.rescale('Robust')
+    #predict_age,MSE,MAE=a.k_Fold(10,LinearRegression())
+    #print(predict_age,MSE,MAE)
+    #predict_age,MSE,MAE=a.k_Fold(10,GaussianProcessRegressor())
+    #a.rescale('Standard')
+    #print(predict_age,MSE,MAE)
