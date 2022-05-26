749cbe2e96d3af9f7925e98e9cef013e3d154bc4
a.corvino@hzdr.de <gelato20>
2022-05-26 21:14:59 +0200

    loos changed

--- BrainAge/deeplearning.py
+++ BrainAge/deeplearning.py
@@ -23,30 +23,33 @@ class Deep:
         (self.df_AS, self.df_TD) = self.util.file_split()
         self.df_TD = self.df_TD.drop(['Site', 'FILE_ID'], axis = 1)
         # Divide the dataset in train, validation and test in a static way
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.df_TD, self.df_TD['AGE_AT_SCAN'], test_size=0.7, random_state=14)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.df_TD, self.df_TD['AGE_AT_SCAN'], test_size=0.3, random_state=14)
         print(self.X_train.shape)
 
     def make_model(self):
         """
         """
         inputs = Input(shape=(424))
-        hidden = Dense(12, activation='relu')(inputs)
+        hidden = Dense(128, activation='relu')(inputs)
         hidden = Dense(12, activation='relu')(hidden)
         hidden = Dense(12, activation='relu')(hidden)
         hidden = Dense(12, activation='relu')(hidden)
         outputs = Dense(1, activation='sigmoid')(hidden)
 
         deepmodel = Model(inputs=inputs, outputs=outputs)
-        deepmodel.compile(loss='binary_crossentropy', optimizer='adam')
+        deepmodel.compile(loss='mean_absolute_error', optimizer='adam', metrics=['MSE'])
         deepmodel.summary()
-        deephistory=deepmodel.fit(self.X_train,self.y_train,validation_split=0.5,epochs=2500,batch_size=128,verbose=0) #CHANGE HERE# Trying increasing number of epochs and changing batch size
+        deephistory=deepmodel.fit(self.X_train,self.y_train,validation_split=0.4,epochs=1000,batch_size=50,verbose=0) #CHANGE HERE# Trying increasing number of epochs and changing batch size
 
-        plt.plot(history.history["val_loss"])
-        plt.plot(history.history["loss"])
-        plt.plot(deephistory.history["val_loss"])
-        plt.plot(deephistory.history["loss"])
+
+        plt.plot(deephistory.history["val_loss"],label='val')
+        plt.plot(deephistory.history["loss"],label='train')
+        plt.legend()
         plt.show()
-        return model
+        # plt.semilogy(deephistory.history['MSE'])
+        # plt.semilogy(deephistory.history['val_MSE'])
+        # plt.show()
+        return deepmodel
 
 if __name__ == "__main__":
     deep=Deep("data/FS_features_ABIDE_males.csv")
