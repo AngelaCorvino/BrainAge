5c1af56fde2df285a61de32b6b38052cea1c58eb
a.corvino@hzdr.de <gelato20>
2022-06-06 13:48:50 +0200

    import libraries added

--- BrainAge/features.py
+++ BrainAge/features.py
@@ -2,8 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from neuroHarmonize import harmonizationLearn
 from neuroCombat import neuroCombat
 
+import statsmodels.api as sm
 
 class Preprocessing:
     """
