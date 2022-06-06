fd3dd40d37cb645d9b3dc33dc4094b2e8a9cc028
naaga96 <91053226+naaga96@users.noreply.github.com>
2022-06-06 23:09:05 +0200

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -47,21 +47,21 @@ class TestBrainAge(unittest.TestCase):
         print(dataframe.shape)
         assert dataframe.shape == (915, 427)
 
-    def test_file_split(self):
-        prep = Preprocessing()
-        dataframe = prep.file_reader(self.data)
-        df_AS, df_TD = prep.file_split(dataframe)
-        assert df_AS.shape == (451, 424)
-        assert df_TD.shape == (464, 424)
+    #def test_file_split(self):
+        #prep = Preprocessing()
+        #dataframe = prep.file_reader(self.data)
+        #df_AS, df_TD = prep.file_split(dataframe)
+        #assert df_AS.shape == (451, 424)
+        #assert df_TD.shape == (464, 424)
 
     def test_feature_selection(self):
         prep = Preprocessing()
         dataframe = prep.file_reader(self.data)
         features, _, _ = prep.feature_selection(dataframe, 'AGE_AT_SCAN', False)
         assert features.shape == (13, )
-        _, df_TD = prep.file_split(dataframe)
-        features_TD, _, _ = prep.feature_selection(df_TD, 'AGE_AT_SCAN', False)
-        assert features_TD.shape == (16, )
+        #_, df_TD = prep.file_split(dataframe)
+        #features_TD, _, _ = prep.feature_selection(df_TD, 'AGE_AT_SCAN', False)
+        #assert features_TD.shape == (16, )
 
 if __name__ == "__main__":
     unittest.main()
