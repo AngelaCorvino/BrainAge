efd34bf905186d8b933078e2602c495aab30d6d2
a.corvino@hzdr.de <gelato20>
2022-06-11 18:29:14 +0200

    Tuming hyperparameter implemented

--- BrainAge/Comparemodel.py
+++ BrainAge/Comparemodel.py
@@ -19,6 +19,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import RobustScaler
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
+from sklearn.model_selection import GridSearchCV
 
 from regression import Regression
 from features import Preprocessing
@@ -92,7 +93,60 @@ def run_gaussianmodel(dataframe,harmonize_list ):
     return
 
 
-run_linearmodel(df, harmonize_list)
+
+def run_randomforest(dataframe,harmonize_list):
+    """
+    Define dictonary in wihich searching the best set of hyperparameters
+    """
+    hyparams = {
+    'Model__n_estimators': [200,300,400,500],
+    'Model__max_features': ['sqrt', 'log2'],
+    'Model__max_depth' : [4,5,6,7,8],
+    'Model__random_state' : [18]
+    }
+    pipe = Pipeline(steps=[('Feature', SelectKBest(score_func=f_classif, k=10)),
+                                    ('Scaler', RobustScaler()),
+                                    ('Model',RandomForestRegressor())])
+    for harmonize_option in harmonize_list:
+        print('Harmonization model:',harmonize_option)
+        dataframe=prep(df, harmonize_option,False)
+        df_AS, df_TD = file_split(dataframe)
+
+        x_train, x_test, y_train, y_test,y_train_class,y_test_class = train_test_split(df_TD.drop(['AGE_AT_SCAN'],axis=1), df_TD['AGE_AT_SCAN'],df_TD['AGE_CLASS'], test_size = .25, random_state = 18)
+        rf_cv = GridSearchCV(pipe, cv=10, n_jobs=-1, param_grid=hyparams ,scoring='roc_auc')
+        """
+         If the estimator is a classifier and y is either binary or multiclass, StratifiedKFold is used. In all other cases, KFold is used.
+        """
+        #rf_cv.fit(df_TD.drop(['AGE_AT_SCAN']), df_TD['AGE_AT_SCAN'])
+        rf_cv.fit(x_train, y_train_class)
+        """
+        Considering that the stratified kfold works better
+        we coul decide to choose the  best hyperparameyer on age class.
+        In this way we can use the stratified k-folf
+        """
+        print('Best parameters are:',rf_cv.best_params_.keys())
+
+        """
+        Now that we have our optimal list of parameters,
+         we can run the basic RandomForestClassifier model using
+         these parameters.
+         This time we fir on age at scan, not age class
+        """
+        rf2 = RandomForestRegressor(rf_cv.best_params_).fit(x_train, y_train)
+        predict_y=rf2.predict(x_test)
+        MSE=mean_squared_error(y_test, predict_y, squared=False)
+        plt.figure(figsize=(10,10))
+        plt.scatter(y_test,predict_y,c='y')
+        plt.xlabel('Ground truth Age(years)')
+        plt.ylabel('Predicted Age(years)')
+        plt.plot(np.linspace(test_age.min(),test_age.max(),100),np.linspace(test_age.min(),test_age.max(),100), c='r', label='Expected prediction line')
+        plt.text(test_age.max()-2,predict_age.max()-2,f'Mean Absolute Error={MSE}',fontsize=14)
+        plt.title('Ground-truth Age versus Predict Age using \n \
+            Random Forest with {} harminization method'.format(harmonize_option))
+        plt.show()
+    return
+
+run_randomforest(df, ['raw'])
 
 
 # #Deep learning
