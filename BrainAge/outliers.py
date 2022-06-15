ebfeac56f4c72e744fc697f978a357b00dd0bb54
naaga96 <agata.minnocci@gmail.com>
2022-06-15 22:42:51 +0200

    final adjustments in outliers.py and implementation of __call__ method

--- BrainAge/outliers.py
+++ BrainAge/outliers.py
@@ -38,12 +38,18 @@ def step_wise(x, N=4, a=100):
 
 
 def gaussian(x, x0, sigma, a):
+    """
+    Gaussian function to use in fit.
+    """
     return (
         a * (1 / np.sqrt(np.pi * sigma**2)) * np.exp(-((x - x0) ** 2) / (sigma**2))
     )
 
 
 def sumgaussian(x, x0, x1, sigma0, sigma1, a, b):
+    """
+    Sum of two gaussian function to possibly use in fit.
+    """
     return gaussian(x, x0, sigma0, a) + gaussian(x, x1, sigma1, b)
 
 
@@ -61,6 +67,15 @@ class Outliers:
         """
         self.X_train = X_train
         self.model = self.make_autoencoder()
+
+    def __call__(self, epochs, nbins, plot=True):
+        """ """
+        self.fit_autoencoder(epochs)
+        indexes = self.outliers(nbins)
+        if plot is True:
+            self.plot_distribution(self.X_train, indexes, "AGE_AT_SCAN")
+        clean = self.clean_dataframe(self.X_train, indexes)
+        return clean
 
     def make_autoencoder(self):
         """Autoencoder trained comparing the output vector with the input features,
@@ -117,23 +132,30 @@ class Outliers:
         xdiscrete_1 = np.linspace(
             test_mae_loss.min() + d_1 / 2, test_mae_loss.max() - d_1 / 2, nbins
         )
-        xcont_1 = np.linspace(test_mae_loss.min(), test_mae_loss.max(), 1000)
         # Plot_test
         plt.figure(1)
         n_1, _, _ = plt.hist(
             x=test_mae_loss, bins=nbins, color="lightskyblue", label="Sample counts"
         )
-        plt.title("Mean Absolute Error Loss", fontsize = 24)
-        plt.xlabel("Test MAE Loss", fontsize = 18)
-        plt.ylabel("Number of Samples", fontsize = 18)
+        plt.title("Mean Absolute Error Loss", fontsize=24)
+        plt.xlabel("Test MAE Loss", fontsize=18)
+        plt.ylabel("Number of Samples", fontsize=18)
 
         # fit as a gaussian
         p0 = [0.4, 0.05, 20]
-        #p0 = [8000, 1000, 10]
-        fit, fitCov = curve_fit(gaussian, xdiscrete_1, n_1, p0 = p0)
+        # p0 = [8000, 1000, 10]
+        fit, fitCov = curve_fit(gaussian, xdiscrete_1, n_1, p0=p0)
         fit_err = np.sqrt(abs(np.diag(fitCov)))
         print(
             f" Test fit parameters: \n x_0 = {fit[0] : .3f} +-{fit_err[0] : .3f}\n sigma_0 = {fit[1] : .3f} +-{fit_err[1] : .3f}\n A = {fit[2] : .3f} +-{fit_err[2] : .3f}\n"
+        )
+        # Plot fit
+        plt.plot(
+            np.linspace(test_mae_loss.min(), test_mae_loss.max(), 1000),
+            gaussian(np.linspace(test_mae_loss.min(), test_mae_loss.max(), 1000), *fit),
+            color="red",
+            linewidth=1,
+            label="fit",
         )
         # fit a sum of gaussians
         # p1 = [0.4, 24, 0.5, 0.1, 20, 5]
@@ -143,18 +165,15 @@ class Outliers:
         #    f" First fit parameters: \n x_0 = {fit[0] : .3f} +-{fit_err[0] : .3f}\n x_1 = {fit[1] : .3f} +-{fit_err[1] : .3f}\n sigma_0 = {fit[2] : .3f} +-{fit_err[2] : .3f}\n sigma_1 = {fit[3] : .3f} +-{fit_err[3] : .3f}\n A = {fit[4] : .3f} +-{fit_err[4] : .3f}\n B = {fit[5] : .3f} +-{fit_err[5] : .3f}\n"
         # )
         # Plot_fit_test
-        plt.plot(
-            xcont_1, gaussian(xcont_1, *fit), color="red", linewidth=1, label="fit"
-        )
         # plt.plot(xcont_1, sumgaussian(xcont_1, *fit), color = 'red', linewidth=1, label='fit')
         plt.show()
 
         def condition(x, fit):
             if x >= (fit[0] + 3 * fit[1]):
-                #return bool(x >= (fit[0] + 3 * fit[1]))
+                # return bool(x >= (fit[0] + 3 * fit[1]))
                 return 1
             if x <= (fit[0] - 3 * fit[1]):
-                #return bool(x <= (fit[0] - 3 * fit[1]))
+                # return bool(x <= (fit[0] - 3 * fit[1]))
                 return 1
             else:
                 return 0
@@ -163,30 +182,41 @@ class Outliers:
         final_outliers = [condition(x, fit) for x in test_mae_loss]
         indexes = list(np.flatnonzero(final_outliers))
         print("Number of final outlier samples: ", np.sum(final_outliers))
-        print("Percentage of outliers: ", np.sum(final_outliers)/len(self.X_train)*100, "%")
-        print("Indices of final outlier samples: ", indexes)
+        print(
+            "Percentage of outliers: ",
+            np.sum(final_outliers) / len(self.X_train) * 100,
+            "%",
+        )
         print("Reconstruction MAE error threshold: {} ".format(np.max(test_mae_loss)))
         return indexes
-        
+
     def plot_distribution(self, dataframe, indexes, feature):
         y = dataframe.iloc[indexes]
-        bins = int(max(y[feature])-min(y[feature]))
+        bins = int(max(y[feature]) - min(y[feature]))
         plt.figure(2)
         n_1, _, _ = plt.hist(
-            x=y[feature], bins=bins, facecolor = "lightskyblue", edgecolor='blue', linewidth=0.5, label="Sample counts"
+            x=y[feature],
+            bins=bins,
+            facecolor="lightskyblue",
+            edgecolor="blue",
+            linewidth=0.5,
+            label="Sample counts",
         )
-        plt.title("Age Distribution of Outliers", fontsize = 24)
+        plt.title("Age Distribution of Outliers", fontsize=24)
         plt.xlabel("Age(years)", fontsize=18)
         plt.ylabel("Number of Samples", fontsize=18)
         return plt.show()
 
-if __name__ == "__main__":
+    def clean_dataframe(self, dataframe, indexes):
+        y = dataframe.iloc[indexes]
+        clean = dataframe.drop(index=y.index)
+        return clean
 
+
+if __name__ == "__main__":
     prep = Preprocessing()
     df = prep.read_file("data/FS_features_ABIDE_males.csv")
     df = prep(df, "raw", False)
     df_AS, df_TD = prep.split_file(df)
     out = Outliers(df_TD)
-    out.fit_autoencoder(epochs = 100)
-    indexes = out.outliers(nbins = 500)
-    out.plot_distribution(df_TD, indexes, 'AGE_AT_SCAN')
+    clean_dataframe = out(epochs=100, nbins=500, plot=True)
