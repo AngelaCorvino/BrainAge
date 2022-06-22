ca6e08d81584f55ec551253b71cdd2f2d04c4ef4
naaga96 <agata.minnocci@gmail.com>
2022-06-22 12:53:01 +0200

    updating socs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,8 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx'
+    'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme'
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
